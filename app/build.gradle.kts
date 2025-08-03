e4041deae3e23d9be1a8598ec5cd0ed348041c96
maazm7d <skmaaz933628@gmail.comm>
2025-08-03 19:05:17 +0530

    v5.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.maazm7d.quickse"
         minSdk = 25
         targetSdk = 36
-        versionCode = 4
-        versionName = "4.1"
+        versionCode = 5
+        versionName = "5.1.1"
     }
 
     signingConfigs {
